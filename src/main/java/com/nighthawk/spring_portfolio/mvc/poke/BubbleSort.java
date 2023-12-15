83d0311497b2ab805a9afd949c81a1f1a9acfaed
Shivansh Goel <shivanshggamer@gmail.com>
2023-12-14 23:51:39 -0800

    Bubble Sort implementation extended to all columns in NFL Data

--- src/main/java/com/nighthawk/spring_portfolio/mvc/poke/BubbleSort.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/poke/BubbleSort.java
@@ -4,30 +4,124 @@ public class BubbleSort {
 	static void bubbleSort(Standings teamStandings[], String sortKey, boolean ascending) {
 		Standings temp;
 		boolean swapped;
-		
+
 		// outer loop
 		for (int i = 0; i < teamStandings.length - 1; i++) {
 			// initialize swapped to false
 			swapped = false;
-			
+
+			/*
+			 * Tm W L W-L% PF PA PD Mov SoS SRS OSRS DSRS
+			 */
 			// inner loop
 			for (int j = 0; j < teamStandings.length - i - 1; j++) {
-				// if ascending order is requested
-				if (ascending) {
-					if (teamStandings[j].getWins() > teamStandings[j + 1].getWins()) {
-						// Swap teamStandings[j] and teamStandings[j+1]
-						temp = teamStandings[j];
-						teamStandings[j] = teamStandings[j + 1];
-						teamStandings[j + 1] = temp;
-						swapped = true;
+				if (sortKey.equals("Tm")) {
+					String firstTeamName = "";
+					String secondTeamName = "";
+
+					firstTeamName = teamStandings[j].getTeamName();
+					secondTeamName = teamStandings[j + 1].getTeamName();
+
+					// if ascending order is requested
+					if (ascending) {
+						if (firstTeamName.compareTo(secondTeamName) > 0) {
+							// Swap teamStandings[j] and teamStandings[j+1]
+							temp = teamStandings[j];
+							teamStandings[j] = teamStandings[j + 1];
+							teamStandings[j + 1] = temp;
+							swapped = true;
+						}
+					} else { // if descending order is requested
+						if (firstTeamName.compareTo(secondTeamName) < 0) {
+							// Swap teamStandings[j] and teamStandings[j+1]
+							temp = teamStandings[j];
+							teamStandings[j] = teamStandings[j + 1];
+							teamStandings[j + 1] = temp;
+							swapped = true;
+						}
 					}
-				} else { // if descending order is requested
-					if (teamStandings[j].getWins() < teamStandings[j + 1].getWins()) {
-						// Swap teamStandings[j] and teamStandings[j+1]
-						temp = teamStandings[j];
-						teamStandings[j] = teamStandings[j + 1];
-						teamStandings[j + 1] = temp;
-						swapped = true;
+				} else if (sortKey.equals("W") || sortKey.equals("L") || sortKey.equals("PF") || sortKey.equals("PA")
+						|| sortKey.equals("PD")) {
+					int firstNum = 0;
+					int secondNum = 0;
+
+					if (sortKey.equals("W")) {
+						firstNum = teamStandings[j].getWins();
+						secondNum = teamStandings[j + 1].getWins();
+					} else if (sortKey.equals("L")) {
+						firstNum = teamStandings[j].getLosses();
+						secondNum = teamStandings[j + 1].getLosses();
+					} else if (sortKey.equals("PA")) {
+						firstNum = teamStandings[j].getPointsAgainst();
+						secondNum = teamStandings[j + 1].getPointsAgainst();
+					} else if (sortKey.equals("PD")) {
+						firstNum = teamStandings[j].getPointsDiff();
+						secondNum = teamStandings[j + 1].getPointsDiff();
+					} else if (sortKey.equals("PF")) {
+						firstNum = teamStandings[j].getPointsFor();
+						secondNum = teamStandings[j + 1].getPointsFor();
+					}
+
+					// if ascending order is requested
+					if (ascending) {
+						if (firstNum > secondNum) {
+							// Swap teamStandings[j] and teamStandings[j+1]
+							temp = teamStandings[j];
+							teamStandings[j] = teamStandings[j + 1];
+							teamStandings[j + 1] = temp;
+							swapped = true;
+						}
+					} else { // if descending order is requested
+						if (firstNum < secondNum) {
+							// Swap teamStandings[j] and teamStandings[j+1]
+							temp = teamStandings[j];
+							teamStandings[j] = teamStandings[j + 1];
+							teamStandings[j + 1] = temp;
+							swapped = true;
+						}
+					}
+				} else if (sortKey.equals("W-L") || sortKey.equals("Mov") || sortKey.equals("SoS")
+						|| sortKey.equals("SRS") || sortKey.equals("OSRS") || sortKey.equals("DSRS")) {
+					double firstNum = 0;
+					double secondNum = 0;
+
+					if (sortKey.equals("W-L")) {
+						firstNum = teamStandings[j].getWlPercentage();
+						secondNum = teamStandings[j + 1].getWlPercentage();
+					} else if (sortKey.equals("Mov")) {
+						firstNum = teamStandings[j].getMarginOfVictory();
+						secondNum = teamStandings[j + 1].getMarginOfVictory();
+					} else if (sortKey.equals("SoS")) {
+						firstNum = teamStandings[j].getSOS();
+						secondNum = teamStandings[j + 1].getSOS();
+					} else if (sortKey.equals("SRS")) {
+						firstNum = teamStandings[j].getSRS();
+						secondNum = teamStandings[j + 1].getSRS();
+					} else if (sortKey.equals("OSRS")) {
+						firstNum = teamStandings[j].getOSRS();
+						secondNum = teamStandings[j + 1].getOSRS();
+					} else if (sortKey.equals("DSRS")) {
+						firstNum = teamStandings[j].getDSRS();
+						secondNum = teamStandings[j + 1].getDSRS();
+					}
+
+					// if ascending order is requested
+					if (ascending) {
+						if (firstNum > secondNum) {
+							// Swap teamStandings[j] and teamStandings[j+1]
+							temp = teamStandings[j];
+							teamStandings[j] = teamStandings[j + 1];
+							teamStandings[j + 1] = temp;
+							swapped = true;
+						}
+					} else { // if descending order is requested
+						if (firstNum < secondNum) {
+							// Swap teamStandings[j] and teamStandings[j+1]
+							temp = teamStandings[j];
+							teamStandings[j] = teamStandings[j + 1];
+							teamStandings[j + 1] = temp;
+							swapped = true;
+						}
 					}
 				}
 			}
@@ -53,7 +147,7 @@ public class BubbleSort {
 		Standings s5 = new Standings("TeamE", 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		Standings s6 = new Standings("TeamF", 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		Standings teamStandings[] = { s6, s1, s3, s2, s5, s4 };
-		BubbleSort.bubbleSort(teamStandings, "wins", false);
+		BubbleSort.bubbleSort(teamStandings, "Tm", false);
 		System.out.println("Sorted: ");
 		BubbleSort.printArray(teamStandings);
 	}
